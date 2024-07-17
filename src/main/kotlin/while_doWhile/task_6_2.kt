853e0f78ea62f320aa92203393addf633d78cc0a
koteev-m <1812.mms@gmail.com>
2024-07-17 16:21:10 +0300

    KT-6-2 without cycle

--- src/main/kotlin/while_doWhile/task_6_2.kt
+++ src/main/kotlin/while_doWhile/task_6_2.kt
@@ -1,13 +1,11 @@
 package while_doWhile
 
 fun main() {
-    var secondsInput = readln().toInt()
-    val second = secondsInput
+    println("Введите количество секунд:")
+    val secondsInput = readln().toInt()
 
-    while (secondsInput > 0){
-        println("Секунд осталось: ${secondsInput--}")
-        Thread.sleep(1000)
-    }
-    println("Прошло $second секунд(-ы)")
+    Thread.sleep(secondsInput * 1000L)
+
+    println("Прошло $secondsInput секунд(-ы)")
 
 }
