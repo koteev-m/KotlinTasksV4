73275a07fd15b5fe6d428f4e5501356f4dcc1b20
koteev-m <1812.mms@gmail.com>
2024-08-12 08:26:17 +0300

    KT-11-4

---
+++ src/main/kotlin/classes_ptoprties/task_11_4.kt
@@ -0,0 +1,29 @@
+package classes_ptoprties
+
+class RecipeCategory(
+    val name: String,
+)
+
+class Ingredient(
+    val name: String,
+    val quantity: String,
+)
+
+class Recipe(
+    val name: String,
+    val numberOfServings: Int,
+    val ingredients: List<Ingredient>,
+    val instruction: String,
+)
+
+
+
+
+//Дизайнер предоставил макет Android-приложения:
+//https://www.figma.com/file/89K5JomHGyIkUFUaBKRBXi/RecipesApp?type=design&node-id=1%3A181&mode=design&t=DsOaqqRfAeCTY2eN-1
+//
+//Для представленных экранов спроектируй следующие сущности для хранения данных:
+//
+//- Категория рецепта
+//- Рецепт
+//- Ингредиент
