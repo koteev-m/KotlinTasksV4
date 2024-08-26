3d45d4dae1cf4d9999935d3c73196c73e5209213
koteev-m <1812.mms@gmail.com>
2024-08-26 10:06:37 +0300

    KT-14-2 add open val

--- src/main/kotlin/inheritance/task_14_2.kt
+++ src/main/kotlin/inheritance/task_14_2.kt
@@ -1,75 +1,81 @@
 package inheritance
 
 open class Liner2(
-    val name: String,
-    val speed: Int,
-    val passengerCapacity: Int,
-    val cargoCapacity: Int,
+    open val name: String,
+    open val speed: Int,
+    open val passengerCapacity: Int,
+    open val cargoCapacity: Int,
 ) {
     open fun info() {
         println("$name: скорость $speed узлов, пассажировместимость $passengerCapacity, грузоподъемность $cargoCapacity тонн")
     }
+
     open fun load() {
-        println("$name выдвигает горизонтальный трап со шкафута")
+        println("$name: выполняет стандартную погрузку")
     }
 }
 
 class PassengerLiner2(
-    name: String = "Пассажирский лайнер"
-) : Liner2(
-    name = name,
-    speed = 30,
-    passengerCapacity = 3000,
-    cargoCapacity = 1000,
-)
+    override val name: String = "Пассажирский лайнер",
+    override val speed: Int = 30,
+    override val passengerCapacity: Int = 3000,
+    override val cargoCapacity: Int = 1000
+) : Liner2(name, speed, passengerCapacity, cargoCapacity) {
+
+    override fun load() {
+        println("$name выдвигает горизонтальный трап со шкафута")
+    }
+}
 
 class CargoShip2(
-    name: String = "Грузовой корабль"
-) : Liner2 (
-    name = name,
-    speed = 20,
-    passengerCapacity = 100,
-    cargoCapacity = 5000,
-) {
+    override val name: String = "Грузовой корабль",
+    override val speed: Int = 20,
+    override val passengerCapacity: Int = 100,
+    override val cargoCapacity: Int = 5000
+) : Liner2(name, speed, passengerCapacity, cargoCapacity) {
+
     override fun load() {
         println("$name активирует погрузочный кран")
     }
 }
 
 class Icebreaker2(
-    name: String = "Ледокол"
-) : Liner2 (
-    name = name,
-    speed = 15,
-    passengerCapacity = 50,
-    cargoCapacity = 70,
-) {
-    fun breakIce() {
-        println("$name колет лёд")
-    }
+    override val name: String = "Ледокол",
+    override val speed: Int = 15,
+    override val passengerCapacity: Int = 50,
+    override val cargoCapacity: Int = 70
+) : Liner2(name, speed, passengerCapacity, cargoCapacity) {
 
     override fun load() {
         println("$name открывает ворота со стороны кормы")
     }
+
+    fun breakIce() {
+        println("$name колет лёд")
+    }
 }
 
-
 fun main() {
-    val liner = PassengerLiner2()
-    liner.info()
-    liner.load()
-
+    val passengerLiner = PassengerLiner2()
     val cargoShip = CargoShip2()
+    val iceBreaker = Icebreaker2()
+
+    passengerLiner.info()
+    passengerLiner.load()
+
     cargoShip.info()
     cargoShip.load()
 
-    val iceBreaker = Icebreaker2()
     iceBreaker.info()
     iceBreaker.load()
     iceBreaker.breakIce()
-
-
 }
+
+
+
+
+
+
 
 
 //Скопируй классы из предыдущей задачи и измени их названия для избегания конфликтов.
